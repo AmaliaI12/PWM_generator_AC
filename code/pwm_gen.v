0fbd105f6c74175ca014b19067a2bd009ae98f75
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-11-15 17:59:04 +0200

    Add combinational logic for PWM generator
    Added combinational logic for the PWM generator in order to determine the
    next state of the pwm signal based on pwm_end, functions, compare1,
    compare2 and counter_val signals.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- code/pwm_gen.v
+++ code/pwm_gen.v
@@ -12,5 +12,35 @@ module pwm_gen (
     // top facing signals
     output pwm_out
 );
-    
+
+    // wire variables that determine the alignment of the pwm signal based on functions
+    wire is_aligned_left = (functions[1:0] == 2'b00);
+    wire is_aligned_right = (functions[1:0] == 2'b01);
+    wire is_unaligned = (functions[1] == 1'b1);
+
+    // reg variables that track the current state and the next state of the pwm signal
+    reg pwm_next_state;
+    reg pwm_current_state;
+
+    assign pwm_out = pwm_curr_state;
+
+    // Combinational logic
+    always @(*) begin
+        if (pwm_en) begin
+            if (is_unaligned) begin
+                pwm_next_state = (count_val >= compare1) && (count_val < compare2);
+            end
+            else if (is_aligned_left) begin
+                pwm_next_state = (count_val < compare1);
+            end
+            else if (is_aligned_right) begin
+                pwm_next_state = (count_val >= compare1);
+            end
+        end
+        else begin
+            pwm_out_next = pwm_current_state;
+        end
+    end
+
+
 endmodule
