5738868e51b087009630378a8cc8f088871e6418
Amalia Ionescu <ionescu.amalia98@gmail.com>
2025-11-19 15:49:40 +0200

    Synchronize external signals with double flip-flop
    Signed-off-by: Amalia Ionescu <ionescu.amalia98@gmail.com>

--- code/spi_bridge.v
+++ code/spi_bridge.v
@@ -13,4 +13,27 @@ module spi_bridge (
     input[7:0] data_out
 );
 
+  // synchronise sclk, cs_n and mosi using two flip flops to avoid metastable state
+    reg sclk1, sclk2;
+    reg cs1, cs2;
+    reg mosi1, mosi2;
+    
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin
+            sclk1 <= 0;
+            sclk2 <= 0;
+            cs1 <= 1;
+            cs2 <= 1;
+            mosi1 <=0;
+            mosi2 <= 0;
+        end else begin
+            sclk1 <= sclk;
+            sclk2 <= sclk1;
+            cs1 <= cs_n;
+            cs2 <= cs1;
+            mosi1 <=mosi;
+            mosi2 <= mosi1;
+        end
+    end
+
 endmodule
